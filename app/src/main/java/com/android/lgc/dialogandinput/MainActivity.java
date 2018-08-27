ed8849c51f0476d7fcecdab739b03041e5ae4043
liguichao <liguichaoshou@163.com>
2018-08-27 14:16:00 +0800

    获取用户名和密码

--- app/src/main/java/com/android/lgc/dialogandinput/MainActivity.java
+++ app/src/main/java/com/android/lgc/dialogandinput/MainActivity.java
@@ -7,12 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     AlertDialog.Builder builder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +32,30 @@ public class MainActivity extends AppCompatActivity {
                 .setTitle(getString(R.string.dialog_title))
                 .setView(msg)
                 .setNegativeButton(getString(R.string.cancel), null)
-                .setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener(){
+                .setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .show();
+    }
+
+    public void loginDialog(View view) {
+        View viewGroup = LayoutInflater.from(MainActivity.this).inflate(R.layout.login_dialog, null);
+        final EditText editTextAccount = (EditText) viewGroup.findViewById(R.id.editTextPassword);
+        final EditText editTextPassword = (EditText) viewGroup.findViewById(R.id.editTextAccount);
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle(getString(R.string.dialog_title))
+                .setView(viewGroup)
+                .setCancelable(false)//设置这个对话框不能被用户按[返回键]而取消掉
+                .setNegativeButton(getString(R.string.cancel), null)
+                .setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String account = editTextAccount.getText().toString();
+                        String password = editTextPassword.getText().toString();
+                        Log.d(TAG, "account:" + account);
+                        Log.d(TAG, "password:" + password);
                     }
                 })
                 .show();
