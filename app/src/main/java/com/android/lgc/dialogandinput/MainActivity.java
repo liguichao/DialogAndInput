a07681cc2bcf68a61cecb9da3b24b2f98b8239e1
liguichao <liguichaoshou@163.com>
2018-08-27 11:48:46 +0800

    实现DialogInterface提示框

--- app/src/main/java/com/android/lgc/dialogandinput/MainActivity.java
+++ app/src/main/java/com/android/lgc/dialogandinput/MainActivity.java
@@ -1,13 +1,38 @@
 package com.android.lgc.dialogandinput;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = MainActivity.class.getSimpleName();
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openInterfaceDialog(View view) {
+        TextView msg = new TextView(this);
+        msg.setText(R.string.dialog_view);
+        msg.setPadding(10, 10, 10, 10);
+        msg.setGravity(Gravity.CENTER);
+        msg.setTextSize(18);
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle(getString(R.string.dialog_title))
+                .setView(msg)
+                .setNegativeButton(getString(R.string.cancel), null)
+                .setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .show();
     }
 }
