e52c6ba46fbcb8710260801644af95f186a480c6
liguichao <liguichaoshou@163.com>
2018-08-28 09:57:43 +0800

    loginDialog的EditText横屏输入时不自动变全屏，loginDialog打开的对话框横竖屏切换时不退出

--- app/src/main/java/com/android/lgc/dialogandinput/MainActivity.java
+++ app/src/main/java/com/android/lgc/dialogandinput/MainActivity.java
@@ -1,6 +1,5 @@
 package com.android.lgc.dialogandinput;
 
-import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,13 +8,13 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,30 +43,39 @@ public class MainActivity extends AppCompatActivity {
         View viewGroup = LayoutInflater.from(MainActivity.this).inflate(R.layout.login_dialog, null);
         final EditText editTextAccount = (EditText) viewGroup.findViewById(R.id.editTextAccount);
         final EditText editTextPassword = (EditText) viewGroup.findViewById(R.id.editTextPassword);
+        final EditText editTextPasswordPrompt = (EditText) viewGroup.findViewById(R.id.editTextPasswordPrompt);
 
-        new AlertDialog.Builder(MainActivity.this)
-                .setTitle(getString(R.string.dialog_title))
-                .setView(viewGroup)
-                .setCancelable(false)//设置这个对话框不能被用户按[返回键]而取消掉
-                .setNegativeButton(getString(R.string.cancel), null)
-                .setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String account = editTextAccount.getText().toString();
-                        String password = editTextPassword.getText().toString();
-                        Log.d(TAG, "account:" + account);
-                        Log.d(TAG, "password:" + password);
-                    }
-                })
-                .show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.dialog_title));
+        builder.setView(viewGroup);
+        builder.setCancelable(false);//设置这个对话框不能被用户按[返回键]而取消掉
+        builder.setNegativeButton(getString(R.string.cancel), null);
+        builder.setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String account = editTextAccount.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String passwordPrompt = editTextPasswordPrompt.getText().toString();
+                Log.d(TAG, "account:" + account);
+                Log.d(TAG, "password:" + password);
+                Log.d(TAG, "passwordPrompt:" + passwordPrompt);
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        WindowManager.LayoutParams layoutParams = alertDialog.getWindow().getAttributes();
+//        layoutParams.width=350;//定义宽度
+//        layoutParams.height=300;//定义高度
+        alertDialog.getWindow().setAttributes(layoutParams);
     }
 
     public void searchDialog(View view) {
         final EditText editText = new EditText(this);
+        editText.setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI);
         new AlertDialog.Builder(this)
                 .setTitle("搜索")
                 .setView(editText)
-                .setNegativeButton("取消",null)
+                .setNegativeButton("取消", null)
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
